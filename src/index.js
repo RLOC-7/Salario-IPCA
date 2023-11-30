0645b74d906bf06d2d28015796edb9fc5a67ac93
RLOC <psnraphael@hotmail.com>
2023-11-30 17:04:39 -0300

    Atualização de dados v2

--- src/index.js
+++ src/index.js
@@ -1,84 +1,59 @@
 import opcao from "readline-sync";
-import { salarioMinimo, inflacao} from './data/dados.js'
+import {salarioMinimo, inflacao} from '../data/dados.js';
 
-console.log("Escolha uma das alternativas: \n")
+console.log("Escolha uma das alternativas: \n");
+console.log("1 - Listar os salários mínimos de 2010 à 2020\n");
+console.log("2 - Listar o índice IPCA de 2010 à 2020\n");
+console.log("3 - Comparação entre o percentual de aumento salarial e o IPCA\n");
 
-console.log("1 - Listar os salários minímos de 2010 à 2020\n")
-console.log("2 - Listar o índice IPCA de 2010 à 2020\n")
-console.log("3 - Comparação entre o percentual de aumento salarial e o IPCA\n")
+let escolha = opcao.question("Digite o número da sua escolha: ");
+escolha = Number(escolha);
 
+const anoLabel = "Ano: ";
+const salarioLabel = "Salário mínimo: ";
+const crescimentoLabel = "Crescimento Salarial: ";
+const ipcaLabel = "Inflação IPCA: ";
 
-let escolha = opcao.question("Digite o numero da sua escolha: ")
-
-escolha = Number(escolha)
-
-let anoLabel = "Ano: "
-let salarioLabel = "Salário mínimo: "
-let crescimentoLabel = "Crescimento Salarial: "
-let ipcaLabel = "Inflação IPCA: "
-
-anoLabel = anoLabel.padEnd(30, '.')
-salarioLabel = salarioLabel.padEnd(30, '.')
-crescimentoLabel = crescimentoLabel.padEnd(30, '.')
-ipcaLabel = ipcaLabel.padEnd(30, '.')
-
+const formatarNumero = (numero) => new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(numero);
 
 switch (escolha) {
     case 1:
-        for (let i = 0; i <= salarioMinimo.length-1; i++) {
-            let ano = salarioMinimo[i].ano
-            let salario = salarioMinimo[i].salario
-            
-            let salarioFormatado = salario.toFixed(2).replace(".",".")
+        for (let i = 0; i < salarioMinimo.length; i++) {
+            const ano = salarioMinimo[i].ano;
+            const salario = salarioMinimo[i].salario;
+            const salarioFormatado = formatarNumero(salario);
 
-            console.log("\n")
-            console.log(anoLabel+ano)
-            console.log(salarioLabel+"R$ "+salarioFormatado)
+            console.log(`\n${anoLabel}${ano}`);
+            console.log(`${salarioLabel}${salarioFormatado}`);
         }
         break;
     case 2:
         for (let i = 0; i < inflacao.length; i++) {
-            let ano = inflacao[i].ano
-            let ipca = inflacao[i].ipca
+            const ano = inflacao[i].ano;
+            const ipca = inflacao[i].ipca;
+            const ipcaFormatado = ipca.toFixed(2).replace(".", ",");
 
-            let ipcaFormatado = ipca.toFixed(2).replace(".",",")
-
-            console.log("\n")
-            console.log(anoLabel+ano)
-            console.log(ipcaLabel+ipcaFormatado+"%")
-
-            
+            console.log(`\n${anoLabel}${ano}`);
+            console.log(`${ipcaLabel}${ipcaFormatado}%`);
         }
         break;
     case 3:
         for (let i = 0; i < salarioMinimo.length; i++) {
-            let ano = salarioMinimo[i].ano;
-            let salario = salarioMinimo[i].salario
-            let percentualCrescimento;
-            let crescimentoFormatado;
-            
-            if (i > 0) {
-                let salarioAnterior = salarioMinimo[i-1].salario
-                let diferenca = salario - salarioAnterior
-                
-                percentualCrescimento = (diferenca / salarioAnterior) * 100
+            const ano = salarioMinimo[i].ano;
+            const salario = salarioMinimo[i].salario;
+            const percentualCrescimento = i > 0 ? ((salario - salarioMinimo[i - 1].salario) / salarioMinimo[i - 1].salario) * 100 : 0;
+            const crescimentoFormatado = percentualCrescimento.toFixed(2).replace(".", ",") + "%";
+            const ipca = inflacao[i].ipca;
 
-                crescimentoFormatado = percentualCrescimento.toFixed(2).replace(".",",")+"%"
-                
-            } else {
-                crescimentoFormatado = "-"
-            }
-            let ipca = inflacao[i].ipca
+            const salarioFormatado = formatarNumero(salario);
+            const ipcaFormatado = ipca.toFixed(2).replace(".", ",");
 
-            let salarioFormatado = salario.toFixed(2).replace(".",",")
-            let ipcaFormatado = ipca.toFixed(2).replace(".",",")
-
-            console.log("\n")
-            console.log(anoLabel+ano)
-            console.log(salarioLabel+"R$ "+salarioFormatado)
+            console.log(`\n${anoLabel}${ano}`);
+            console.log(`${salarioLabel}${salarioFormatado}`);
+            console.log(`${crescimentoLabel}${crescimentoFormatado}`);
         }
         break;
     default:
-        console.log("Opção Inválida!")
+        console.log("Opção Inválida!");
         break;
 }
