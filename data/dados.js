51be23c7e89d40db2a69e21cf23c55085fff475e
RLOC <psnraphael@hotmail.com>
2023-11-30 15:50:49 -0300

    Atualização

---
+++ data/dados.js
@@ -0,0 +1,29 @@
+let salarioMinimo = [
+    {ano: 2010, salario: 510.00},
+    {ano: 2011, salario: 545.00},
+    {ano: 2012, salario: 622.00},
+    {ano: 2013, salario: 678.00},
+    {ano: 2014, salario: 724.00},
+    {ano: 2015, salario: 788.00},
+    {ano: 2016, salario: 880.00},
+    {ano: 2017, salario: 937.00},
+    {ano: 2018, salario: 954.00},
+    {ano: 2019, salario: 998.00},
+    {ano: 2020, salario: 1045.00}
+];
+
+let inflacao = [
+    {ano: 2010, ipca: 5.91},
+    {ano: 2011, ipca: 6.50},
+    {ano: 2012, ipca: 5.84},
+    {ano: 2013, ipca: 5.91},
+    {ano: 2014, ipca: 6.41},
+    {ano: 2015, ipca: 10.67},
+    {ano: 2016, ipca: 6.29},
+    {ano: 2017, ipca: 2.95},
+    {ano: 2018, ipca: 3.75},
+    {ano: 2019, ipca: 4.31},
+    {ano: 2020, ipca: 4.52}
+];
+
+export default {salarioMinimo, inflacao}
